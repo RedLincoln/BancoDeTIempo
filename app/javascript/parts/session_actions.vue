d22aa71d27b36d163c04800309f04d9aaadb6bfe
RedLincoln <miguel_lcp@hotmail.com>
2020-08-13 23:37:00 +0000

    green: vue session_actions no props is not logged in

---
+++ app/javascript/parts/session_actions.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div id="session_actions">
+    <a class="login_button">Iniciar sesión</a>
+    <a class="sign_up_button">Registrarse</a>
+  </div>
+</template>
+
+<script>
+// import store from '@/store'
+export default {
+  data: function() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
