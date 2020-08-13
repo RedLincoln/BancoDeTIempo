de9e8262ed95bf28a5c590b41aecf32cfd95236c
RedLincoln <miguel_lcp@hotmail.com>
2020-08-13 11:57:29 +0000

    red: navbar sign_up button when not logged in

--- spec/system/navbar_spec.rb
+++ spec/system/navbar_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe 'Navbar', type: :system do
 
     expect(current_path).to eq(new_user_session_path)
   end
+
+  it 'have sign_up button when user is not logged in', :aggregate_failures do
+    visit root_path
+
+    within('#navbar #session_actions') do
+      expect(page).to have_selector('.sign_up_button', text: 'Registrarse')
+      click_on('Registrarse')
+    end
+
+    expect(current_path).to eq(new_user_registration_path)
+  end
 end
