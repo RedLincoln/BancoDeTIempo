ccb909d37ce0787dc869c2a505f0e91dc238ae3c
RedLincoln <miguel_lcp@hotmail.com>
2020-08-16 14:34:31 +0000

    fixing errors in can be created test

--- spec/system/service_spec.rb
+++ spec/system/service_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Service', type: :system, js: true do
 
     fill_in('service_name', with: 'Pintar')
     fill_in('service_description', with: 'pintar tanto exteriores como interiores')
-    click_in('Crear servicio')
+    click_on('Crear servicio')
 
-    expect(current_path).to eq(servic_path(1))
+    expect(current_path).to eq(service_path(1))
 
     expect(page).to have_selector('.service_name', text: 'Pintar')
     expect(page).to have_selector('.service_description', text: 'pintar tanto exteriores como interiores')
