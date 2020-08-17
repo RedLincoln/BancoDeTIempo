e2def22d30f496e350246be07a9a18120617b10a
RedLincoln <miguel_lcp@hotmail.com>
2020-08-17 23:50:36 +0000

    green: CreateService indicates when service is valid

--- app/workflow/create_services.rb
+++ app/workflow/create_services.rb
@@ -1,14 +1,24 @@
 class CreateServices
-  attr_accessor :name, :description, :user, :service
+  attr_accessor :name, :description, :user, :valid, :service
 
   def initialize(name: "", description: "", user: User.new)
     @name = name
     @description = description
     @user = user
+    @valid = false
   end
 
   def build
     self.service = Service.new(name: @name, description: @description, user: @user)
+  end
+
+  def create
+    build
+    @valid = service.save
+  end
+
+  def valid?
+    @valid
   end
 
 end
