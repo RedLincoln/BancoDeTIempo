28fdd8bc768010820df84b903d9049fd1220f55e
RedLincoln <miguel_lcp@hotmail.com>
2020-08-17 23:28:00 +0000

    red:Service validations should validate that :name is case-sensitively unique within the scope of :user_id

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Service, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:name).scoped_to(:user_id)}
   end
 end
