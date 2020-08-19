eb85e84289608c309e9a68487bacfbc4cd75199e
RedLincoln <miguel_lcp@hotmail.com>
2020-08-19 23:39:10 +0000

    mend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import Route from "../routes";
-
 console.log('Hello World from Webpacker')
 
 import './session_actions'
