25ef24ab2552af86319aa4c3d05ebb2cf28640d3
RedLincoln <miguel_lcp@hotmail.com>
2020-08-19 23:49:13 +0000

    red: component navigation_dropdown initial state

--- spec/javascript/navigation_dropdown.spec.js
+++ spec/javascript/navigation_dropdown.spec.js
@@ -2,9 +2,16 @@ import { shallowMount } from "@vue/test-utils";
 import navigation_dropdown from "../../app/javascript/parts/navigation_dropdown";
 
 describe('navigation_dropdown.vue', ()=>{
-    it('initial state is no show', () =>  {
-        const wrapper = shallowMount(navigation_dropdown)
 
-        
+    it('initial state is no show', () =>  {
+        const props = {
+            title: 'title'
+        }
+        const wrapper = shallowMount(navigation_dropdown, {
+            propsData: props
+        })
+
+        expect(wrapper.find('.dropdown_title').text()).toBe(props.title)
+        expect(wrapper.find('.dropdown_content').exists()).toBeFalsy()
     })
 })
