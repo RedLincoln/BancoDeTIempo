b2432f89a79ce76f3eb74e05ca20c440419fa202
RedLincoln <miguel_lcp@hotmail.com>
2020-08-19 19:57:24 +0000

    green: integration services can be editted

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_service, only: [:update, :edit]
 
   def index
     @services = Service.all
@@ -10,16 +11,23 @@ class ServicesController < ApplicationController
   end
 
   def edit
-    @service = current_user.find_service(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @service.update(service_params)
+        format.html { redirect_to root_path }
+      else
+        format.js 'create'
+      end
+    end
   end
 
   def create
-    @workflow = CreateServices.new(name: params[:service][:name],
-                                   description: params[:service][:description],
-                                   user: current_user)
+    @workflow = CreateServices.new(service_params)
     @workflow.create
     if @workflow.valid?
-      redirect_to services_url
+      redirect_to root_path
     else
       @service = @workflow.service
       respond_to do |format|
@@ -27,5 +35,20 @@ class ServicesController < ApplicationController
         format.js
       end
     end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :description)
+    {
+        name: params[:service][:name],
+        description: params[:service][:description],
+        user: current_user
+    }
+  end
+
+  def set_service
+    @service = current_user.find_service(params[:id])
   end
 end
