e3fcbc57601ede087c015cc9e62fa73e2c39fae3
RedLincoln <miguel_lcp@hotmail.com>
2020-08-21 16:25:10 +0000

    green: workflow CreateService properly indicates when is valid

--- app/workflow/create_services.rb
+++ app/workflow/create_services.rb
@@ -1,15 +1,21 @@
 class CreateServices
   attr_accessor :name, :description, :user, :valid, :service
 
-  def initialize(name: "", description: "", user: User.new)
+  def initialize(name: "", description: "", user: User.new, category_id: -1)
     @name = name
     @description = description
+    @category_id = category_id
     @user = user
     @valid = false
   end
 
   def build
-    self.service = @user.services.build(name: @name, description: @description)
+    begin
+      category = Category.find(@category_id)
+      rescue ActiveRecord::RecordNotFound
+      category = Category.new
+    end
+    self.service = @user.services.build(name: @name, description: @description, category: category)
   end
 
   def create
