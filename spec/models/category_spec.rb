07357744e8ba60f8a8666523002ae2559d3683cd
RedLincoln <miguel_lcp@hotmail.com>
2020-08-21 22:00:20 +0000

    red: Category model name should be unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Category, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:supcategory) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
