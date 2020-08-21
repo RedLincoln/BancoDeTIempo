4dbb32b05f8bc78dcad2f0576e70020af223d7f3
RedLincoln <miguel_lcp@hotmail.com>
2020-08-21 16:53:30 +0000

    green: Service is not valid when user is nil

--- app/workflow/create_services.rb
+++ app/workflow/create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices
   attr_accessor :name, :description, :user, :valid, :service
 
-  def initialize(name: "", description: "", user: User.new, category_id: -1)
+  def initialize(name: "", description: "", user: nil, category_id: -1)
     @name = name
     @description = description
     @category_id = category_id
@@ -10,7 +10,11 @@ class CreateServices
   end
 
   def build
-    self.service = @user.services.build(name: @name, description: @description, category: category)
+    if user.nil?
+      self.service = Service.new(name: @name, description: @description, category: @category)
+    else
+      self.service = @user.services.build(name: @name, description: @description, category: category)
+    end
   end
 
   def create
