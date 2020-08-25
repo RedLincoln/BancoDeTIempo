b200ff745d33203182948209785a670e83c43884
RedLincoln <miguel_lcp@hotmail.com>
2020-08-25 13:57:02 +0000

    transaction_form compoennt refactor

--- spec/javascript/transaction_form.spec.js
+++ spec/javascript/transaction_form.spec.js
@@ -3,22 +3,28 @@ import transaction_form from "../../app/javascript/parts/transaction_form";
 
 describe ('transaction_form.vue',  ()=>{
 
-    it ('show content', ()=>{
-        const wrapper = shallowMount(transaction_form, {
-            data(){
-                return {
-                    show: true
-                }
-            }
-
-        })
-
-        expect(wrapper.find('.form').exists()).toBeTruthy()
-    })
-
     it ('initially hide content', ()=>{
         const wrapper = shallowMount(transaction_form)
 
         expect(wrapper.find('.form').exists()).toBeFalsy()
+    })
+
+    describe('', () => {
+        let wrapper
+
+        beforeEach(()=> {
+            wrapper = shallowMount(transaction_form, {
+                data(){
+                    return {
+                        show: true
+                    }
+                }
+
+            })
+        })
+
+        it ('show content', ()=>{
+            expect(wrapper.find('.form').exists()).toBeTruthy()
+        })
     })
 })
