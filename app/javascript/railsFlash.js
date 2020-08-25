4cc3e0950ddc34f925b3372e0f1d0acaac384395
RedLincoln <miguel_lcp@hotmail.com>
2020-08-25 19:32:13 +0000

    green: javascript flash message alert without prior flash message

--- app/javascript/railsFlash.js
+++ app/javascript/railsFlash.js
@@ -4,5 +4,13 @@ export default {
         notice.classList.add('flash-message', 'notice')
         notice.innerHTML = content
         document.getElementById('flash-messages').appendChild(notice)
-    }
+    },
+
+    alert: (content) => {
+        const alert =  document.createElement('div')
+        alert.classList.add('flash-message', 'alert')
+        alert.innerHTML = content
+        document.getElementById('flash-messages').appendChild(alert)
+    },
+
 }
