02a151dce84f685c25ba3ec1c80badc8c7758c7c
RedLincoln <miguel_lcp@hotmail.com>
2020-08-25 14:20:50 +0000

    red: trasaction_form component sends peittion once clicked in send petit

--- spec/javascript/transaction_form.spec.js
+++ spec/javascript/transaction_form.spec.js
@@ -1,4 +1,6 @@
 import { shallowMount} from "@vue/test-utils";
+import MockAdapter from "axios-mock-adapter";
+import axios from 'axios'
 import transaction_form from "../../app/javascript/parts/transaction_form";
 
 describe ('transaction_form.vue',  ()=>{
@@ -18,15 +20,22 @@ describe ('transaction_form.vue',  ()=>{
 
     describe('', () => {
         let wrapper
+        let axiosMock
+        const $createTransactionPath =  '/transactions'
 
         beforeEach(()=> {
+            jest.clearAllMocks()
+            axiosMock  = new MockAdapter(axios)
+
             wrapper = shallowMount(transaction_form, {
                 data(){
                     return {
                         show: true
                     }
+                },
+                mocks: {
+                    $createTransactionPath: $createTransactionPath
                 }
-
             })
         })
 
@@ -37,6 +46,18 @@ describe ('transaction_form.vue',  ()=>{
         it ('hides content when click on button', async() => {
             await wrapper.find('.open_petition').trigger('click')
             expect(wrapper.find('.form').exists()).toBeFalsy()
+        })
+
+        it ('sends petition once clicked in send petition', () => {
+            const spy = jest.spyOn(axios, 'post')
+            axiosMock.onPost($createTransactionPath, {
+                transaction: {
+                    datetime: wrapper.find('.time_petition'),
+                    additional_information: wrapper.find('.additional_information')
+                }
+            }).reply(200)
+
+            expect(spy).toBeCalledTimes(1)
         })
     })
 })
