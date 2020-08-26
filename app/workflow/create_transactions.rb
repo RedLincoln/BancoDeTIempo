a52b6c4b43c77ce8cb1d9fe6c26cf208118078cf
RedLincoln <miguel_lcp@hotmail.com>
2020-08-26 17:16:41 +0000

    green: transaction status default is pending

--- app/workflow/create_transactions.rb
+++ app/workflow/create_transactions.rb
@@ -1,16 +1,26 @@
 class CreateTransactions
-  attr_accessor :datetime, :addition_information, :service, :client, :transaction
+  attr_accessor :datetime, :addition_information, :service, :client, :success, :transaction
 
   def initialize(datetime: '', addition_information: '', service_id: -1, client: nil)
     @datetime = datetime
     @addition_information = addition_information
     @service = get_service(service_id)
     @client = client
+    @success = false
   end
 
   def build
     self.transaction = Transaction.new(datetime: @datetime, addition_information: @addition_information,
                                        service: @service, client: @client)
+  end
+
+  def create
+    build
+    @success = transaction.save
+  end
+
+  def success?
+    @success
   end
 
   private
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Transaction, type: :model do
     it { should belong_to(:client).class_name(:User) }
     it { should belong_to(:service) }
   end
+
+  describe 'default' do
+
+    it 'status' do
+      expect(Transaction.new.status).to eq('pending')
+    end
+  end
 end
