6d5be646c502cbbaac4aa99067490acc0490a441
RedLincoln <miguel_lcp@hotmail.com>
2020-08-26 17:29:02 +0000

    red: model Transaction validation service owner can not be the client

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,15 @@
 class Transaction < ApplicationRecord
   belongs_to :client, class_name: :User
   belongs_to :service
+
+  validate :service_owner_can_not_be_the_client
+
+  private
+
+  def service_owner_can_not_be_the_client
+    nil_checker = service.nil? && client.nil?
+    if nil_checker || service.user == client
+      errors.add(:client, 'client and service owner can not be the same')
+    end
+  end
 end
