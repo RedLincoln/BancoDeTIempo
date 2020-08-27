faab13751e74c3b6f76b19504ac2384959c08dcc
RedLincoln <miguel_lcp@hotmail.com>
2020-08-27 15:38:54 +0000

    red: vue form helper get array Post params

--- spec/javascript/formHelper.spec.js
+++ spec/javascript/formHelper.spec.js
@@ -17,4 +17,25 @@ describe('form helper', ()=>{
             expect(expected).toEqual({ name: 'the_value' })
         })
     })
+
+    describe('array parameters', () => {
+        let params
+
+        beforeEach(() => {
+            let form = document.createElement('form')
+            form.innerHTML = `<input name="name[1]" value="the_value1">
+                              <input name="name[2]" value="the_value2">`
+            params = form.elements
+        })
+
+        it('are converted properly', () => {
+            const expected = formHelper.getPostParams(params)
+
+            expect(expected).toEqual({
+                name: {
+                    1: 'the_value1',
+                    2: 'the_value2'
+                }})
+        })
+    })
 })
