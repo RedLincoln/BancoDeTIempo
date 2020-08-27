8605c390980eaf95ebfd72c0f7bbc84f1792dbbf
RedLincoln <miguel_lcp@hotmail.com>
2020-08-27 16:22:22 +0000

    green: vue form helper get array Post params

--- app/javascript/formHelper.js
+++ app/javascript/formHelper.js
@@ -1,9 +1,19 @@
+let arrayRegex = /(.+)\[(.+)\]/
+
 export default {
     getPostParams: (elements) => {
         const result = {}
         for(let i = 0; i < elements.length; i++){
             const element =  elements.item(i)
-            result[element.name] = element.value
+            if (arrayRegex.test(element.name)){
+                const [empty, key, value] = element.name.match(arrayRegex)
+                if (!result.hasOwnProperty(key)){
+                    result[key] = {}
+                }
+                result[key][value] = element.value
+            }else{
+                result[element.name] = element.value
+            }
         }
         return result
     }
