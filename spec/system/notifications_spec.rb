f346b69ef3ee9d2699cdf9004ae8aaa58a013f07
RedLincoln <miguel_lcp@hotmail.com>
2020-08-31 19:30:43 +0000

    red: integrations Notifications: show notification when service petitions are made

---
+++ spec/system/notifications_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Notifications', type: :system, js: true do
+  let(:user) { create(:user) }
+  let(:service) { create(:service, user: user)}
+  let(:transaction) { create(:transaction, service: service) }
+
+  before(:each) do
+    sign_in user
+    visit root_path
+    transaction
+  end
+
+  it 'show notification when service petitions are made' do
+    visit services_path
+    notifications = find('#notifications')
+
+    within notifications do
+      expect(page).to have_selector('#notifications-counter', text: '1')
+    end
+
+    notifications.click
+    expect(page).to have_selector('#notifications-list')
+
+    within '#notifications-list' do
+      expect(page).to have_selector('.notification .target', text: service.name)
+    end
+  end
+end
