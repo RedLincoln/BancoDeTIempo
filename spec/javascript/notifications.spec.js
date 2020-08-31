03d742d2b27039ae2fb6290f8207628e620cf9e1
RedLincoln <miguel_lcp@hotmail.com>
2020-08-31 19:47:20 +0000

    red: notificaiont.vue dropdown clicking button shows and hides content

--- spec/javascript/notifications.spec.js
+++ spec/javascript/notifications.spec.js
@@ -14,6 +14,16 @@ describe('notifcations.vue', () => {
             expect(wrapper.find('.toggle-button').exists()).toBeTruthy()
             expect(wrapper.find('.dropdown-content').exists()).toBeFalsy()
         })
+
+        it('clicking toggle button show and hides content', async ()=>{
+            await wrapper.find('.toggle-button').trigger('click')
+
+            expect(wrapper.find('.dropdown-content').exists()).toBeTruthy()
+
+            await wrapper.find('.toggle-button').trigger('click')
+
+            expect(wrapper.find('.dropdown-content').exists()).toBeFalsy()
+        })
     })
 
 })
