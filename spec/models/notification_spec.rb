fc6ba4c83f299b05110c036a3b965cd5f5666ab4
RedLincoln <miguel_lcp@hotmail.com>
2020-09-01 12:40:53 +0000

    red notifications model expect target to not be empty

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Notification, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:message)}
+    it { should validate_presence_of(:target)}
   end
 
 end
