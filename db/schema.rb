b9f38cd77e81553df4bd00283fc6d84423bec174
RedLincoln <miguel_lcp@hotmail.com>
2020-09-02 13:49:50 +0000

    green: model Notification seen field default value is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_124420) do
+ActiveRecord::Schema.define(version: 2020_09_02_134847) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_124420) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "seen", default: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
