d62a331bd00c38e14db958ff6cb9e7fe1ff3ff06
RedLincoln <miguel_lcp@hotmail.com>
2020-09-03 14:44:19 +0000

    green: datetime_picker.vue initial date selection is present Date

--- app/javascript/parts/DatetimePicker/DatetimePicker.vue
+++ app/javascript/parts/DatetimePicker/DatetimePicker.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="datetime_picker">
     <div class="datetime-input"></div>
+    <div class="date-picker">
+      <p>
+        <span class="month">{{ month }}</span>
+        <span class="year">{{ year }}</span>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import date_utils from "../../date_utils";
+
 export default {
   data: function() {
-    return {};
+    return {
+      month: "",
+      year: "",
+    };
+  },
+  created() {
+    const date = new Date(Date.now());
+    this.month = date_utils.getMonth(date.getMonth());
+    this.year = date.getFullYear();
   },
 };
 </script>
--- spec/javascript/datetime_picker.spec.js
+++ spec/javascript/datetime_picker.spec.js
@@ -4,21 +4,31 @@ import DatetimePicker from "../../app/javascript/parts/DatetimePicker/DatetimePi
 // Spanish testing
 
 describe("DatetimePicker.vue", () => {
+  const RealDate = Date.now;
+
+  afterEach(() => {
+    global.Date.now = RealDate;
+  });
+
   it("initial state have no datetime selected", () => {
     const wrapper = shallowMount(DatetimePicker);
     expect(wrapper.find(".datetime-input").text()).toBe("");
   });
 
   it("initial date selection is present Date", async () => {
+    global.Date.now = jest.fn(() => {
+      return new Date("August 19, 1975 23:15:30").valueOf();
+    });
+
     const wrapper = shallowMount(DatetimePicker);
+
+    const date = new Date(Date.now());
 
     await wrapper.find(".datetime-input").trigger("click");
 
-    const date = new Date("August 19, 1975 23:15:30");
-
-    const datePicker = wrapper.find(".date-pricker");
+    const datePicker = wrapper.find(".date-picker");
 
     expect(datePicker.find(".month").text()).toBe("Agosto");
-    expect(datePicker.find(".year").text()).toBe(date.getFullYear());
+    expect(datePicker.find(".year").text()).toBe(date.getFullYear().toString());
   });
 });
