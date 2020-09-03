65fddf2844934e70516b6ca1401067bedab4c329
RedLincoln <miguel_lcp@hotmail.com>
2020-09-03 16:38:00 +0000

    red: date_utils getNextMonthDate properly gets next month in same year

---
+++ spec/javascript/date_util.spec.js
@@ -0,0 +1,23 @@
+import date_utils from "../../app/javascript/date_utils";
+
+describe("date_utils", ()=>{
+
+    describe("getNextMonthDate", ()=>{
+        const RealDate = Date.now;
+
+        afterEach(() => {
+            global.Date.now = RealDate;
+        });
+
+        it("properly gets next month in same year", ()=>{
+            global.Date.now = jest.fn(() => {
+                return new Date("February 15, 1975 23:15:30").valueOf();
+            });
+
+            expect(date_utils.getNextMonthDate(Date.now()).getMilliseconds())
+                .toBe(new Date('March 1, 1975').getMilliseconds())
+        })
+
+
+    })
+})
