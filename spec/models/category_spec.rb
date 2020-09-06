1d72d04373bbb8a837f5130e850d12253cc11230
RedLincoln <miguel_lcp@hotmail.com>
2020-09-06 23:13:15 +0000

    green: Category model have have services

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
 
+  describe 'associations' do
+    it { should have_many(:services) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:supcategory) }
