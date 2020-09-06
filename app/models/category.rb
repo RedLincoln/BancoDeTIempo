4e2fa398c69b489ffdb3f4bff22ae794a45f4bb0
RedLincoln <miguel_lcp@hotmail.com>
2020-09-06 20:07:44 +0000

    green: Category to_json format

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,10 @@ class Category < ApplicationRecord
 
   validates :name, uniqueness: { scope: :supcategory}
 
+  def as_json(*)
+    super(:only => [:id, :name, :supcategory])
+  end
+
   def self.grouped_by_supcategory
     hash = Hash.new{ |h,k| h[k]=[]}
     Category.all.each do |category|
