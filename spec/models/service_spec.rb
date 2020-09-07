52ce116f81112dbf2d081739cc910ed6d0606426
RedLincoln <miguel_lcp@hotmail.com>
2020-09-07 07:48:32 +0000

    red: Service pagination: nil page return page 1

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -100,5 +100,9 @@ RSpec.describe Service, type: :model do
       expect(Service.all.pagination(page: 3).to_a).to eql([])
       expect(Service.all.pagination(page: 0).to_a).to eql([])
     end
+
+    it "nil page return page 1" do
+      expect(Service.all.pagination(page: nil).to_a).to eql([service1, service2, service3, service4, service5])
+    end
   end
 end
