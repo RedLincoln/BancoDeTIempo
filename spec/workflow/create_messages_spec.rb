14d3bd5fb8a9bc2055a0bfcb9f54d3904f8458b1
RedLincoln <miguel_lcp@hotmail.com>
2020-09-10 23:15:47 +0000

    red: create_messages Workflow successfuly created valid messages

--- spec/workflow/create_messages_spec.rb
+++ spec/workflow/create_messages_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe CreateMessages do
     expect(creator.message.service_petition).to eq(service_petition)
   end
 
+  it 'successfully create valid messages' do
+    creator = CreateMessages.new(message: 'This is a Message', author: author, service_petition: service_petition)
+    creator.create
+
+    expect(creator).to be_successful
+    expect(Message.exists?(message: 'This is a Message')).to eq(true)
+  end
+
 end
