ba147ad873459b1fc6e40156911a28ca2629a280
RedLincoln <miguel_lcp@hotmail.com>
2020-09-10 23:12:01 +0000

    green: create_messages Workflow initial values

--- app/workflow/create_messages.rb
+++ app/workflow/create_messages.rb
@@ -1,3 +1,14 @@
 class CreateMessages
+  attr_accessor :author, :service_petition, :message_content, :message
+
+  def initialize(author:, service_petition:, message:)
+    @author = author
+    @message_content = message
+    @service_petition = service_petition
+  end
+
+  def build
+    @message = Message.new(author: @author, service_petition: @service_petition, message: @message_content)
+  end
 
 end
