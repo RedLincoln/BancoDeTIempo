5aa017091fa84f41e2f11efb36defa7527dc7d14
RedLincoln <miguel_lcp@hotmail.com>
2020-10-04 02:18:08 +0000

    red: Login.vue have necesary elements

--- spec/javascript/views/Login.spec.js
+++ spec/javascript/views/Login.spec.js
@@ -6,17 +6,24 @@ import Login from "../../../app/javascript/views/Login.vue";
 describe("Login.vue", () => {
   const localVue = createLocalVue();
   let vuetify;
+  let wrapper;
 
   beforeEach(() => {
     vuetify = new Vuetify();
-  });
-
-  it("renders properly", () => {
-    const wrapper = mount(Login, {
+    wrapper = mount(Login, {
       localVue,
       vuetify,
     });
+  });
 
+  it("renders properly", () => {
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("have the necesary elements", () => {
+    expect(wrapper.find('[data-testid="login-form"]').exists()).toBe(true);
+    expect(wrapper.find('[data-testid="email-field"]').exists()).toBe(true);
+    expect(wrapper.find('[data-testid="password-field"]').exists()).toBe(true);
+    expect(wrapper.find('[data-testid="sumit-btn"]').exists()).toBe(true);
   });
 });
