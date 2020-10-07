faaa9a3ac687df5a2210729cb93c5eeba5728514
RedLincoln <miguel_lcp@hotmail.com>
2020-10-07 14:09:00 +0000

    green: formRules email must contain a doamin of atleast length 2

--- app/javascript/utils.js
+++ app/javascript/utils.js
@@ -1,4 +1,5 @@
 export const rules = {
   required: (value) => !!value || "Campo obligatorio",
-  email: (value) => /[a-z]{1}[a-z0-9]*@/.test(value) || "Email incorrecto",
+  email: (value) =>
+    /[a-z]{1}[a-z0-9]*@[a-z]*.[a-z]{2,+}/.test(value) || "Email incorrecto",
 };
