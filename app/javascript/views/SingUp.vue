020a1eaf1bcf5721269e9efeaf62e6c9d580bd6b
RedLincoln <miguel_lcp@hotmail.com>
2020-10-07 16:06:28 +0000

    green: SignUp.vue have all the necesary fields

--- app/javascript/views/SingUp.vue
+++ app/javascript/views/SingUp.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <v-form>
+      <v-text-field data-testid="name-field"> </v-text-field>
+      <v-text-field data-testid="email-field"></v-text-field>
+      <v-file-input data-testid="avatar-field"></v-file-input>
+      <v-textarea data-testid="information-field"> </v-textarea>
+    </v-form>
+  </div>
 </template>
 
 <script>
