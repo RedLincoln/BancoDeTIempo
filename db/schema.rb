9beadfef0d1165f090ec47c658b6ca8492c5ef32
RedLincoln <miguel_lcp@hotmail.com>
2020-10-08 19:08:15 +0000

    green: model User default confirmed is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_015328) do
+ActiveRecord::Schema.define(version: 2020_10_08_190737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_015328) do
     t.string "role", default: "standard"
     t.integer "balance", default: 5
     t.text "information"
+    t.boolean "confirmed", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
